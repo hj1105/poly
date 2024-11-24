e482151d39121e58b876258794800ba463f950eb
hj1105 <b1ack_night@naver.com>
2024-11-25 00:58:57 +0900

    first commit

---
+++ src/main/kotlin/com/example/poly/PolyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.poly
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PolyApplication
+
+fun main(args: Array<String>) {
+	runApplication<PolyApplication>(*args)
+}
